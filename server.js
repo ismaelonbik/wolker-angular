12aa92ec19b8c5bc4762b00c630cfeed77a604f2
Ismael Castillo <ismaelcastillo@onbik.com>
2019-01-30 12:58:46 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+console.log('dentro de server.js');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/pruebaPwa2'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/pruebaPwa2/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
